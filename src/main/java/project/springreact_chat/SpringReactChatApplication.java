76c95325afe750f34b98b0fedde8c5cacff0868f
guseoh <guseoh@naver.com>
2026-03-28 12:42:19 +0900

    Project Settings

---
+++ src/main/java/project/springreact_chat/SpringReactChatApplication.java
@@ -0,0 +1,13 @@
+package project.springreact_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/springreact_chat/SpringReactChatApplicationTests.java
@@ -0,0 +1,13 @@
+package project.springreact_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
