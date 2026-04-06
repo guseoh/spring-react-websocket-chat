4849ced617fcda56fd1df904a7b4ea0bc3e7ff0a
guseoh <81723538+guseoh@users.noreply.github.com>
2026-04-06 18:16:38 +0900

    초기 설정
    * feat: Entity 생성
    
    - ChatMessage
    - ChatRoom
    - ChatRoomParticipant
    - Member
    
    * feat: Repository 뼈대 작성

---
+++ src/main/java/project/springreact_chat/domain/ChatRoomParticipant.java
@@ -0,0 +1,35 @@
+package project.springreact_chat.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class ChatRoomParticipant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", nullable = false)
+    private ChatRoom chatRoom;
+
+    @CreatedDate
+    private LocalDateTime joinedAt;
+}
