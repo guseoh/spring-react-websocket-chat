a2ef550be7e90e01fd01136c7732d4a227f6f9df
guseoh <81723538+guseoh@users.noreply.github.com>
2026-04-08 17:05:20 +0900

    feat: Log 구현 및 설정
    feat: Log 기능 구현

--- src/main/java/project/springreact_chat/config/ChatWebSocketHandler.java
+++ src/main/java/project/springreact_chat/config/ChatWebSocketHandler.java
@@ -1,6 +1,7 @@
 package project.springreact_chat.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -8,6 +9,7 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 import project.springreact_chat.service.ChatWebSocketService;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ChatWebSocketHandler extends TextWebSocketHandler {
@@ -15,18 +17,22 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
     private final ChatWebSocketService chatWebSocketService;
 
     @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("웹소켓 연결 시작 - sessionId={}", session.getId());
         chatWebSocketService.connect(session);
     }
 
     @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-            chatWebSocketService.handleMessage(session, message.getPayload());
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        log.info("웹소켓 연결 종료 - sessionId={}. code={}, reason={}",
+                session.getId(), status.getCode(), status.getReason());
+        chatWebSocketService.disconnect(session);
     }
 
     @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        chatWebSocketService.disconnect(session);
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        log.info("메시지 수신 - sessionId={}, payload={}", session.getId(), message.getPayload());
+        chatWebSocketService.handleMessage(session, message.getPayload());
     }
 
     @Override
